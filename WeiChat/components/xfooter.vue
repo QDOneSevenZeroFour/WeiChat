d9635f88ea4186373a3ddbc6a71ce6a00fee2d35
1508686036 <1508686036@qq.com>
2017-11-11 17:57:09 +0800

    第五次修改

--- WeiChat/components/xfooter.vue
+++ WeiChat/components/xfooter.vue
@@ -1,39 +1,36 @@
 <template>
-
 	<div>
 		<router-view></router-view>
-		<footer class="app-footer _line-fine" style="" _v-c8294afa="">
-			<div class="_effect" _v-c8294afa="">
-				<nav _v-26a78bfd="" _v-c8294afa="">
+		<footer class="app-footer _line-fine" style="" >
+			<div class="_effect" >
+				<nav  >
 					<a href="#/index/xheader/chat">
-						<dl _v-26a78bfd="" class="v-link-active"> <dt class="iconfont icon-wechat" _v-26a78bfd=""> <i _v-26a78bfd="" class="_news-count">12</i> </dt>
-							<dd _v-26a78bfd="">微信</dd>
+						<dl  class="v-link-active"> <dt class="iconfont icon-wechat" > <i  class="_news-count">12</i> </dt>
+							<dd >微信</dd>
 						</dl>
 					</a>
 					<a href="#/index/contact">
-						<dl _v-26a78bfd=""> <dt class="iconfont icon-contact" _v-26a78bfd="">  </dt>
-							<dd _v-26a78bfd="">通讯录</dd>
+						<dl > <dt class="iconfont icon-contact" >  </dt>
+							<dd >通讯录</dd>
 						</dl>
 					</a>
 					<a href="#/index/find">
-						<dl _v-26a78bfd=""> <dt class="iconfont icon-find" _v-26a78bfd=""> <i _v-26a78bfd="" class="_news-dot">99</i> </dt>
-							<dd _v-26a78bfd="">发现</dd>
+						<dl > <dt class="iconfont icon-find" > <i  class="_news-dot">99</i> </dt>
+							<dd >发现</dd>
 						</dl>
 					</a>
 					<a href="#/index/me">
-						<dl _v-26a78bfd=""> <dt class="iconfont icon-me" _v-26a78bfd="">  </dt>
-							<dd _v-26a78bfd="">我</dd>
+						<dl > <dt class="iconfont icon-me" >  </dt>
+							<dd >我</dd>
 						</dl>
 					</a>
 				</nav>
 			</div>
 		</footer>
-
 	</div>
 </template>
 
 <script>
-	
 </script>
 
 <style scoped>
@@ -49,5 +46,4 @@
 		display: inline-block;
 		margin: 0 33px;
 	}
-	
 </style>
